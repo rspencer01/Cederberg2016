rspencer01/Cederberg2016

commit 4bf72012e2f5bcca28f3da56f7835dc0dddb7294

Faster `writeInt` method

src/Compass/sseg.c
@@ -112,7 +112,6 @@ void writeMessage(int message)
 /// @param n The value to display
 void writeInt(int n)
 {
-  writeClear();
   int d = digits(n);
   if (d > 4)
   {
@@ -134,6 +133,8 @@ void writeInt(int n)
   {
     if (i < d)
       displays[i] = SSEG_NUMBERS[n%10];
+    else
+      displays[i] = 0xff;
     n/=10;
   }
 }

commit c56ca70710946eee7d07dc6475b5131e6cddf0cd

Bugfix for `writeInt`
Fixes a bug introduced in 4bf7201 that prevents the `-` from
displaying for negative numebrs.

src/Compass/sseg.c
@@ -111,6 +111,7 @@ void writeInt(int n)
     writeHex(0xFFFF);
     return;
   }
+  int minusPlace = 5;
   if (n < 0)
   {
     if (d>3)
@@ -118,16 +119,21 @@ void writeInt(int n)
       writeHex(0xEEEE);
       return;
     }
-    displays[d] = SSEG_MINUS;
+    minusPlace = d;
     n = -n;
   }
-  int i = 0;
-  for (; i < 4; i++)
+  int i;
+  for (i = 0; i < 4; i++)
   {
     if (i < d)
       displays[i] = SSEG_NUMBERS[n%10];
     else
-      displays[i] = 0xff;
+    {
+      if (i != minusPlace)
+        displays[i] = 0xff;
+      else
+        displays[i] = SSEG_MINUS;
+    }
     n/=10;
   }
 }